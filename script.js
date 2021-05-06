37033cf3e05968a1397d2f3e509751c2b8068ac9
Peter Pan <petarzivkovic85@gmail.com>
2021-05-06 01:54:07 -0700

    run ajax call to the openwethermap api

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ var APIKey = "67990312b903cf578e099efc23a6844f"
 
 var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" +
 "q=Puyallup, WA, USA&appid=" + APIKey;
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+})
+.then(function(response) {
+
+    console.log(queryURL);
+
+    console.log(response);
+
+    $(".city").html("<h1>" + response.name + "Weather Details by Peter the Great</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").html("Humidity: " + response.main.humidity);
+})
